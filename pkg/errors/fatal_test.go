cc930f1960d586c6fc0f1fd3ef1065b72054c5e6
Tim Holm <tim.holm@nitric.io>
2022-03-29 13:12:43 +1100

    test(errors): Add unit tests for the errors package.

---
+++ pkg/errors/fatal_test.go
@@ -0,0 +1,38 @@
+// Copyright 2021 Nitric Pty Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package errors
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Errors", func() {
+	When("creating a new Fatal() error", func() {
+		err := Fatal("test")
+
+		It("should create a new FatalError with the provided msg", func() {
+			Expect(err).To(BeEquivalentTo(&FatalError{
+				msg: "test",
+			}))
+		})
+
+		When("calling Error() on the FatalError", func() {
+			It("should return the msg with fatal: prefix", func() {
+				Expect(err.Error()).To(Equal("fatal: test"))
+			})
+		})
+	})
+})
